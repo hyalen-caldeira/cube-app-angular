b5fcce107dc3b75737179bceab8e8b96fe4f7d56
Hyalen Caldeira <hyalen.moreira@gmail.com>
2022-04-08 15:04:33 -0400

    Added test for UserService

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
-import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 describe('UserService', () => {
   let service: UserService;
+  let http: HttpClient;
+  let controller: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,9 +21,34 @@ describe('UserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(UserService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  fit('should update user when http put methos is called', () => {
+    const mockData = {
+      id: 1,
+      name: 'Any name'
+    }
+
+    const headers = new HttpHeaders()
+      .set('content-type', 'application/vnd.spring-template.user.v1+json')
+      .set('accept', 'application/vnd.spring-template.user.v1+json')
+      .set('Access-Control-Allow-Origin', '*');
+
+    service.update(1, mockData).subscribe((res) => {
+      console.log("I'm here ...");
+      expect(res).toEqual(mockData);
+    })
+
+    const req = controller.expectOne('http://localhost:8080/api/users/1');
+    req.flush(mockData);
+
+    expect(req.request.body).toEqual(mockData);
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.headers).toEqual(headers);
   });
 });
